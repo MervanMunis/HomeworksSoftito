007e8a765aced20250f7e520119969548fa90677
MervanMunis <mervan.munis@gmail.com>
2024-05-27 12:10:45 +0300

    Seaching Algorithm is Arranged

--- ProgramSearching.cs
+++ ProgramSearching.cs
@@ -6,7 +6,47 @@ namespace Work
     {
         public static void Main(string[] args)
         {
-            
+            Console.Write("Enter the string: ");
+            char[] text = Console.ReadLine()
+                                 .ToLower()
+                                 .ToCharArray();
+
+            Console.Write("Searching string:  ");
+            char[] search = Console.ReadLine()
+                                   .ToLower()
+                                   .ToCharArray();
+
+            int startIndex = -1;
+            bool found = false;
+
+            for (int i = 0; i <= text.Length - search.Length; i++)
+            {
+                found = true;
+                for (int j = 0; j < search.Length; j++)
+                {
+                    if (text[i + j] != search[j])
+                    {
+                        found = false;
+                        break;
+                    }
+                }
+
+                if (found)
+                {
+                    startIndex = i;
+                    break;
+                }
+            }
+
+            if (found)
+            {
+                Console.WriteLine("The string you searched for starts at index: " + startIndex);
+            }
+            else
+            {
+                Console.WriteLine("The string you searched for was not found.");
+            }
+            Console.ReadKey();
         }
     }
 }
